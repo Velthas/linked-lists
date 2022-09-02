9cfb98f36818dd0e6b3ee56c8844d6e09abdd802
Velthas <damianobattaglia97@gmail.com>
2022-09-02 20:24:36 +0200

    Added method size, head, tail, at

--- src/linked-list.js
+++ src/linked-list.js
@@ -19,9 +19,48 @@ const linkedListMethods = {
     // Start traversing from the head (duh)
     traverseAndAppend(head);
   },
-};
+  prepend(value) {
+    const head = this; // Keep a reference to head for clarity
+    const newNode = CreateNode(); // Create a new node
 
-const nodeMethods = {};
+    newNode.value = head.value; // Make the new node be a copy of head
+    newNode.next = head.next; // which will now be the second node in the list
+
+    head.value = value; // Change the head value and next props.
+    head.next = newNode; // this way linList methods are retained
+  },
+  size() {
+    const size = (function traverseAndCount(node) {
+      // Recursively traverse linked list
+      if (node.next === null) return 1; // Base case will be reached at end of list
+      return 1 + traverseAndCount(node.next); // after which we just add one for each node traversed
+    }(this));
+    return size;
+  },
+  head() {
+    return this;
+  },
+  tail() {
+    if (this.next === null) return this; // Head is tail if there is only one node
+    let listNode = this; // Get a reference to head for clarity
+    while (listNode.next !== null) {
+      // Sprinkle in some iteration for good measure
+      listNode = listNode.next;
+    }
+    return listNode; // Once the loop exits, the remaining node will be the tail
+  },
+  at(index) {
+    // Recursively traverse the list until index is reached
+    function returnAt(indexs, node) {
+      if (indexs === 0) {
+        return node; // When index = 0 we have the right node, which we return per specification
+      } if (indexs > 0 && node.next === null) {
+        return 'Sorry pal'; // Cheeky, just in case someone tries to look farther than they can
+      } return returnAt(indexs - 1, node.next); // Recursive step
+    }
+    return returnAt(index, this); // Initiate recursion
+  },
+};
 
 // Factory for linked list
 function LinkedList(headValue) {
